3b2d85fa8a6635008f10d2693833ada1dc194244
David Hsieh <dcchsieh@gmail.com>
2015-04-13 00:31:30 +1000

    * alter path of the test file, find in root.

--- GlobalX.Console.ContactSort.Tests/BusinessLogic/FileBusinessLogicTest.cs
+++ GlobalX.Console.ContactSort.Tests/BusinessLogic/FileBusinessLogicTest.cs
@@ -1,9 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 using GlobalX.Console.ContactSort.BusinessLogic.File;
 using GlobalX.Console.ContactSort.Common.Domain;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NUnit.Framework;
 using Assert = NUnit.Framework.Assert;
+using File = GlobalX.Console.ContactSort.Common.Domain.File;
 
 namespace GlobalX.Console.ContactSort.Tests.BusinessLogic
 {
@@ -55,6 +60,13 @@ namespace GlobalX.Console.ContactSort.Tests.BusinessLogic
             //Assert
             Assert.IsTrue(result == "contact-sorted.txt");
         }
+        //static string GetTestDataFolder(string testDataFolder)
+        //{
+        //    string startupPath = Assembly.GetCallingAssembly().assembly.GetManifestResourceStream(resourceName);
+        //    var pathItems = startupPath.Split(Path.DirectorySeparatorChar);
+        //    string projectPath = String.Join(Path.DirectorySeparatorChar.ToString(), pathItems.Take(pathItems.Length - 3));
+        //    return Path.Combine(projectPath, testDataFolder);
+        //}
 
         [Test]
         public void WhenGivenFileWillGenerateProperFileMetadata()
@@ -62,7 +74,8 @@ namespace GlobalX.Console.ContactSort.Tests.BusinessLogic
             //Arrange
 
             //Act
-            var result = _fileBusinessLogic.GenerateFileMetadata("TestData\\contact.txt");
+            string path = AppDomain.CurrentDomain.BaseDirectory;
+            var result = _fileBusinessLogic.GenerateFileMetadata("contact.txt");
 
             //Assert
             Assert.IsTrue(result.FileName == "contact");
@@ -75,7 +88,7 @@ namespace GlobalX.Console.ContactSort.Tests.BusinessLogic
             //Arrange
 
             //Act
-            var result = _fileBusinessLogic.LoadFile("TestData\\contact.txt");
+            var result = _fileBusinessLogic.LoadFile("contact.txt");
 
             //Assert
             Assert.IsTrue(result == "BAKER, THEODORE\r\nSMITH, ANDREW\r\nKENT, MADISON\r\nSMITH, FREDRICK\r\n");
