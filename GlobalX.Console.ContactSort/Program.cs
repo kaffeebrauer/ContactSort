5e67669120df1fc4a4a7e648950110234faf28d3
David Hsieh <dcchsieh@gmail.com>
2015-04-12 18:36:52 +1000

    * initial commit
    * created the main console app project
    * created accompanying test project.

---
+++ GlobalX.Console.ContactSort/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobalX.Console.ContactSort
+{
+    public class Program
+    {
+        private static IContainer _container;
+
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
